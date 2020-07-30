bc9eeb162f72873fb1381240ecc8d943786ac918
grimechristopher <54651065+grimechristopher@users.noreply.github.com>
2020-07-30 15:01:51 -0700

    Changed meta tag

--- header.php
+++ header.php
@@ -9,8 +9,9 @@
     
     <meta property="og:title" content="<?php bloginfo('name'); ?>">
     <meta property="og:type" content="website">
+    <meta property="og:description" content="<?=bloginfo('description')?>">
     <meta property="og:url" content="">
-    <meta property="og:image" content="">
+    <meta property="og:image" content="icon.png">
 
     <link rel="manifest" href="site.webmanifest">
     <link rel="apple-touch-icon" href="icon.png">
